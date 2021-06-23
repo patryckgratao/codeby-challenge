fa1074ff18766b03f7b5c583790ffabd6f490a11
Patryck Gratão <patryck@polichat.com.br>
2021-06-22 22:52:47 -0300

    :recycle: Sorting out List component to individual component

---
+++ src/components/List/List.component.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+
+import {
+  Container,
+  Item,
+  ImageContainer,
+  DetailsContainer,
+  ChocoName,
+  OriginalPrice,
+  NewPrice,
+} from "./List.styles";
+
+const List = ({ items }) => {
+  const itemsFormatted = items?.map((item) => (
+    <Item key={item.id}>
+      <ImageContainer>
+        <img src={item.imageUrl} alt={item.name} />
+      </ImageContainer>
+
+      <DetailsContainer>
+        <ChocoName>{item.name}</ChocoName>
+        <OriginalPrice>R$ {item.price}</OriginalPrice>
+        <NewPrice>R$ {item.sellingPrice}</NewPrice>
+      </DetailsContainer>
+    </Item>
+  ));
+
+  return <Container>{itemsFormatted}</Container>;
+};
+
+export default List;
---
+++ src/components/List/List.styles.jsx
@@ -0,0 +1,48 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  padding: 0.4rem 0.8rem;
+  /* max-height: 100rem; */
+  overflow-y: auto;
+  border-bottom: 2px solid #cacaca;
+`;
+
+export const Item = styled.div`
+  display: grid;
+  grid-template-columns: 1fr 11fr;
+  margin: 0.5rem;
+`;
+
+export const ImageContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  width: 100px;
+  height: 100px;
+  border: 1px solid #d2d2d2;
+
+  & img {
+    max-width: 100%;
+    max-height: 100%;
+  }
+`;
+
+export const DetailsContainer = styled.div`
+  margin-left: 1rem;
+`;
+
+export const ChocoName = styled.span`
+  display: block;
+  font-weight: bold;
+  margin-bottom: 0.5rem;
+`;
+
+export const OriginalPrice = styled.div`
+  font-size: 0.8rem;
+  color: #b0afae;
+`;
+
+export const NewPrice = styled.div`
+  color: #202020;
+`;
